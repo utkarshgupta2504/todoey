3c3224d203194e284c531e1b211809e668171a61
utkarshgupta2504 <utkarshgupta2504@gmail.com>
2020-07-24 18:27:43 +0530

    Added reminder switch

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey/models/task_data.dart';
 
-class AddTaskScreen extends StatelessWidget {
+class AddTaskScreen extends StatefulWidget {
+  @override
+  _AddTaskScreenState createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
   final taskController = TextEditingController();
 
   String currTask;
+  bool remindMe = false;
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +61,28 @@ class AddTaskScreen extends StatelessWidget {
               ),
               SizedBox(
                 height: 10.0,
+              ),
+              SwitchListTile(
+                value: remindMe,
+                title: Text('Reminder'),
+                onChanged: (newValue) {
+                  setState(() {
+                    remindMe = newValue;
+                  });
+                },
+                subtitle: Text('Remind me about this item'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                  child: remindMe
+                      ? Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : null),
+              SizedBox(
+                height: remindMe ? 10.0 : 0.0,
               ),
               FlatButton(
                 color: Colors.lightBlueAccent,
